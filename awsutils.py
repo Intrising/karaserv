29a8d5dee504c9b8ad7cced1a6364a38e8a43770
Tim Wu <changtimwu@gmail.com>
2011-01-20 05:28:50 +0000

    rename & simplify the initialiation procedure.

---
+++ awsutils.py
@@ -0,0 +1,23 @@
+import boto                                                                     
+from boto.s3.key import Key
+
+def aws_connect():
+    import ConfigParser
+    config = ConfigParser.RawConfigParser()
+    config.read('aws.cfg')
+    access_key=config.get('Credentials', 'access_key')
+    secret_key=config.get('Credentials', 'secret_key')
+    return boto.connect_s3( access_key, secret_key)
+
+def aws_get_mvurl( v, fmt):
+    k = mvbkt.get_key('mvsong/{0}/{1}.mp4'.format( fmt, v));
+    return k.generate_url( 60)
+ 
+def aws_close():
+    mvbkt.close()
+    s3conn.close()
+
+s3conn= aws_connect()
+mvbkt = s3conn.get_bucket('mdsmv')
+
+
