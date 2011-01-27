b04cb478e8b14fb22292acc9942dc86c8d187922
Tim Wu <changtimwu@gmail.com>
2011-01-27 07:54:52 +0000

    * integrate actual database query code * you can query by item index only.  query by item string is obsoleted.

--- songqrys.py
+++ songqrys.py
@@ -1,52 +1,84 @@
 import intlmsgs
+from songdb import systemdb
+
 class qrybase:
-	datas=[]
-	def items( self):
-		return self.datas
-	def query( self, itemth):
-		return self.func( self.datas[itemth])
+    datas=[]
+    def items( self):
+        return self.datas
+    def query( self, d):
+        if isinstance( d, str):
+            r = self.func( int(d))
+        else:
+            r = self.func(d)
+        return r    
 
 class qry_singer(qrybase):
     def __init__(self):
-		self.datas = [ 'male', 'female', 'band']
+        self.datas = [ 'male', 'female']
     def func( self, d):
-        return { 'rtype':'singer', 'rdata':['blah']}
-			
+        gs=['M', 'F']
+        singers = systemdb.qsingers( 'gender', gs[d])
+        return { 'rtype':'singer', 'rdata':singers}
+            
 class qry_bopomofo( qrybase):
     def __init__(self):
         bpmf=intlmsgs.gettext('bopomofo_list')
         self.datas = [ ch for ch in bpmf]
     def func( self, d):
-		return { 'rtype':'song', 'rdata':['blah']}
-
-class qry_chinese(qrybase):
-    def __init__(self):
-        self.datas = [ str(nch)+' chars' for nch in xrange(1,11)]
-    def func( self, d):
-		return { 'rtype':'song', 'rdata':['blah']}
+        return { 'rtype':'song', 'rdata':[]}
 
 class qry_alpha(qrybase):
     def __init__(self):
         self.datas = [chr(i) for i in xrange(ord('a'), ord('z')+1)]
     def func( self, d):
-		return { 'rtype':'song', 'rdata':['blah']}
+        songs = systemdb.mquery( { 'lang':3, 'nwords': d+1})
+        return { 'rtype':'song', 'rdata':songs}
+
+
+class qry_chinese(qrybase):
+    def __init__(self):
+        self.datas = [ str(nch)+' chars' for nch in xrange(1,11)]
+    def func( self, d):
+        songs = systemdb.mquery( { 'lang':1, 'nwords': d+1})
+        return { 'rtype':'song', 'rdata':songs}
 
 class qry_taiwanese(qrybase):
     def __init__(self):
         self.datas = [ str(nch)+' chars' for nch in xrange(1,11)]
     def func( self, d):
-   		return { 'rtype':'song', 'rdata':['blah']}
+        songs = systemdb.mquery( { 'lang':2, 'nwords': d+1})
+        return { 'rtype':'song', 'rdata':songs}
 
 class qry_english(qrybase):
     def __init__(self):
-        self.datas = [ str(nch)+' words' for nch in xrange(1,11)]
+        self.datas = [ 'all']
     def func( self, d):
-   		return { 'rtype':'song', 'rdata':['blah']}
+        songs = systemdb.mquery( { 'lang':3 })
+        return { 'rtype':'song', 'rdata':songs}
 
+class qry_japanese(qrybase):
+    def __init__(self):
+        self.datas = [ 'all']
+    def func( self, d):
+        songs = systemdb.mquery( { 'lang':4})
+        return { 'rtype':'song', 'rdata':songs}
+
+class qry_cantonese(qrybase):
+    def __init__(self):
+        self.datas = [ str(nch)+' chars' for nch in xrange(1,11)]
+    def func( self, d):
+        songs = systemdb.mquery( { 'lang':5, 'nwords': d+1})
+        return { 'rtype':'song', 'rdata':songs}
+
+class qry_children(qrybase):
+    def __init__(self):
+        self.datas = ['all']
+    def func( self, d):
+        songs = systemdb.mquery( { 'lang':6})
+        return { 'rtype':'song', 'rdata':songs}
 
 if __name__ == '__main__':
-	qry = qry_singer()
-	d = qry.items()
-	print d
-	qry.query(1)
-	qry.func( 'ff')
+    qry = qry_singer()
+    d = qry.items()
+    print d
+    print qry.query( 1)
